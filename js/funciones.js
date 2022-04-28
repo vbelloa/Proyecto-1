78cec150b8110e678233414d67fc524dcdf9727b
Vicente Bello Ambario <57588852+vbelloa@users.noreply.github.com>
2022-04-28 03:33:55 -0500

    Primer Commit

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(listo);
+
+
+function listo()
+{
+	jQuery(".hamb").click(function(e){
+		e.preventDefault();
+		jQuery("header .container nav").toggleClass("open");
+
+		jQuery(".hamb i").toggleClass("lid fa-x");		
+
+	});
+
+	jQuery("header .container nav a").click(function(){
+
+		jQuery("header .container nav").removeClass("open");
+		jQuery(".hamb i").removeClass("lid fa-x");	
+		
+		var dev = jQuery(this).attr("href");
+
+		jQuery("html,body").animate({
+
+			"scrollTop": jQuery(dev).offset().top -76
+		})
+	})
+}
